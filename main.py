64792f106086276913e21820ee8ec2507c055680
CJenkins <c.e.jenkins001@gmail.com>
2024-03-14 17:17:49 +0000

    Separate colour change
    Arrows now change individually, when their button is pressed (Up Arrow, Down Arrow, Left Arrow, Right Arrow)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 #set up the display
 WIDTH, HEIGHT = 600, 500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -19,11 +20,8 @@ ARROW_HEIGHT, ARROW_WIDTH = 96,96
 #set up the images
 
 frames = ['Assets/default_arrow.png','Assets/green_arrow.png', 'Assets/blue_arrow.png', 'Assets/red_arrow.png','Assets/pink_arrow.png']
-active_frame_up = 0
-active_frame_down = 0
-active_frame_right = 0
-active_frame_left = 0
-mode = 0
+active_frame_up, active_frame_down, active_frame_right, active_frame_left = 0, 0, 0, 0
+mode, mode1, mode2, mode3 = 0, 0, 0, 0
 count = 0
 
 
@@ -53,32 +51,38 @@ def draw_window():
 def update_arrow(mod, counter):
 
      act = 0
+     counter = 0
 
      if counter >= 60:
           counter = 0
+
      if mod == 1:
           if counter < 59:
                act = 1
           if counter >= 59:
                act = 0
+
      if mod == 2:
           if counter < 59:
                act = 2
           if counter >= 59:
                act = 0
+
      if mod == 3:
           if counter < 59:
                act = 3
           if counter >= 59:
                act = 0
+
      if mod == 4:
           if counter < 59:
                act = 4
           if counter >= 59:
                act = 0
-     counter += 1
-     return act, counter
 
+     counter += 1
+
+     return act, counter
 
 #Handles functions & other operations
         
@@ -90,24 +94,28 @@ while run:
 
      clock.tick(FPS)
      active_frame_up, count = update_arrow(mode, count)
-     
-
+     active_frame_down, count = update_arrow(mode1, count)
+     active_frame_left, count = update_arrow(mode2, count)
+     active_frame_right, count = update_arrow(mode3, count)
 
      for event in pygame.event.get():
           if event.type == pygame.QUIT:
                run = False
         
      keys_pressed = pygame.key.get_pressed()
-     if keys_pressed[pygame.K_DOWN]: #Down arrow
+     if keys_pressed[pygame.K_UP]: #UP arrow
           mode = 1
-     if keys_pressed[pygame.K_UP]: #Up arrow
-           mode = 2
+     if keys_pressed[pygame.K_DOWN]: #DOWN arrow
+           mode1 = 2
      if keys_pressed[pygame.K_LEFT]: #Left Arrow
-           mode = 3
+           mode2 = 3
      if keys_pressed[pygame.K_RIGHT]: #Right Arrow
-          mode = 4
+          mode3 = 4
      if event.type == pygame.KEYUP:
           mode = 0
+          mode1 = 0
+          mode2 = 0
+          mode3 = 0
 
      draw_window()
 
